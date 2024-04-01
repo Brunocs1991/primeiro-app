5e9ac772c9ca9d2303e270a000a2cf561d7f5892
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-04-01 19:08:33 -0300

    Trabalhando com useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Nome from "./components/Nome";
+import {useState} from "react";
 
 function App() {
+
+    const [nome, setNome] = useState('Sujeito Programador')
+
+    function handlerCheckName(nome){
+        setNome(nome)
+    }
+
     return (
         <div>
             <h1>Component App</h1>
-            <Nome nome={"Bruno"} idade={10}/>
             <br/>
-            <Nome nome={"Joaquim"} idade={20}/>
+            <h2>Olá: {nome}</h2>
+            <button onClick={() => handlerCheckName("Bruno Costa")}>
+                Mudar Nome
+            </button>
         </div>
     );
 }
